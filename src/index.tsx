516d3d6d46b15de42f793c3b6edba1421b19197c
Caleb Norris <paintballerz15@gmail.com>
2023-05-16 18:11:01 -0700

    Updated BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
