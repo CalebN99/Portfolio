dae56bf52f4137c50160630762133eadcb98232f
Caleb Norris <paintballerz15@gmail.com>
2023-05-17 22:27:58 -0700

    Interests rewording

--- src/components/main/Interests.tsx
+++ src/components/main/Interests.tsx
@@ -10,28 +10,29 @@ function Interests() {
         <div className="project">
           <h2>Auto Detailing / Cars </h2>
           <p>
-            Auto Detailing became something I love doing and do as a job since I
-            graduated high school. My first job out of high school was auto
-            detailing for my brothers company, and I still do to this day. I do
-            it all, from basic interior details and exterior washes, to high end
-            paint corrections and ceramic coatings. I'm also a lover of cars, so
-            a huge benefit of this hobby & job is I get to work on a lot cars I
-            probably would never even get to sit in! A long with many great
-            opportunities such as being able to attend the SEMA show in Las
-            Vegas every year.
+            Ever since I graduated from high school, I developed a deep passion
+            for auto detailing, which eventually turned into my profession. My
+            initial job right after high school was working in auto detailing
+            for my brother's company. I still work for him and even started
+            building software for the company for my capstone. I offer a wide
+            range of services, from basic interior cleaning and exterior washes
+            to advanced paint corrections and application of ceramic coatings.
+            Being an ardent car enthusiast, one of the major perks of this hobby
+            and occupation is the opportunity to work on a diverse array of
+            vehicles that I might never have even had the chance to sit in.
+            Moreover, I've been fortunate enough to take part in remarkable
+            experiences such as attending the annual SEMA show in Las Vegas.
           </p>
 
-          <Carousel id="child_carousel" >
+          <Carousel id="child_carousel">
             <Carousel.Item>
               <img
-                
                 src={require("../../assets/auto_detail/auto_1.jpeg")}
                 alt="First slide"
               />
             </Carousel.Item>
             <Carousel.Item>
               <img
-                
                 src={require("../../assets/auto_detail/auto_2.JPG")}
                 alt="Second slide"
               />
@@ -40,66 +41,65 @@ function Interests() {
         </div>
 
         <div className="project">
-            <h2>
-              Fitness / Being Active
-            </h2>
-            <p>
-              Fitness is very important to me. I had a huge mentality change in the summer of 2020, and ended up dieting and working out, losing 95lbs in a year.
-              This is by far one of my greatest achievements. Most of my activity included cardio, biking, hiking, jogging, etc. However lately I'm focused
-              on building muscle trying to achieve a full body recomposition. 
-            </p>
+          <h2>Fitness / Being Active</h2>
+          <p>
+            Fitness holds tremendous significance in my life. In the summer of
+            2020, I experienced a profound shift in my mindset, leading me to
+            embark on a journey of dieting and exercise. Over the course of a
+            year, I managed to shed an impressive 95 pounds, a feat that stands
+            as one of my most remarkable accomplishments. Initially, my
+            activities primarily revolved around cardio exercises such as
+            biking, hiking, and jogging. However, more recently, my focus has
+            shifted towards muscle building, as I strive to attain a complete
+            body recomposition.
+          </p>
 
-            <Carousel id="child_carousel">
+          <Carousel id="child_carousel">
+            <Carousel.Item>
+              <img
+                src={require("../../assets/fitness/fitness_1.jpeg")}
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src={require("../../assets/fitness/fitness_2.jpeg")}
+                alt="First slide"
+              />
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        <div className="project">
+          <h2>PC Gaming</h2>
+          <p>
+            Since my earliest memories, I have been an avid gamer, starting with
+            PC gaming. Growing up, my family and friends would gather for LAN
+            parties, and during those times, I also gained knowledge in computer
+            building and maintenance. Among the games I have devoted most of my
+            time to are World of Warcraft, Counter-Strike, and League of
+            Legends. However, I am open to exploring and playing almost any game
+            that catches my interest or that my friends are into. I take great
+            pleasure in assembling and upgrading my computer to enhance the
+            performance of my games, as it combines multiple hobbies into one.
+            Currently, my PC specifications include an RTX 3070, Ryzen 3600x,
+            32GB DDR4 RAM, two 500GB SSD's, and a 2TB HDD.
+          </p>
 
-              <Carousel.Item>
-                <img
-                  
-                  src={require("../../assets/fitness/fitness_1.jpeg")}
-                  alt="First slide"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  
-                  src={require("../../assets/fitness/fitness_2.jpeg")}
-                  alt="First slide"
-                />
-              </Carousel.Item>
-            </Carousel>
-          </div>
-          <div className="project">
-              <h2>PC Gaming</h2>
-              <p>
-                I've been gaming since I could remember, started out PC gaming
-                even! I grew up doing lan parties with my family and family
-                friends, a long with learning how to build and work on
-                computers. My main games that I've played the most is World of
-                Warcraft, Counter Strike, and League of Legends. However, I'm
-                open and do play almost anything that seems appealing to me or
-                whatever my friends are playing. I love being able to build and
-                upgrade my computer to increase performance of my games, it's
-                many hobbies in one! Current specs: RTX 3070, Ryzen 3600x, 32gbs
-                DDR4, 1TB SSD, 2GB HDD
-              </p>
-
-              <Carousel id="child_carousel">
-                <Carousel.Item>
-                  <img
-                   
-                    src={require("../../assets/gaming/gaming_1.jpeg")}
-                    alt="First slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    
-                    src={require("../../assets/gaming/gaming_2.png")}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-              </Carousel>
-            </div>
-  
+          <Carousel id="child_carousel">
+            <Carousel.Item>
+              <img
+                src={require("../../assets/gaming/gaming_1.jpeg")}
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src={require("../../assets/gaming/gaming_2.png")}
+                alt="Second slide"
+              />
+            </Carousel.Item>
+          </Carousel>
+        </div>
       </div>
     </div>
   );
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -81,34 +81,6 @@ function Nav() {
           </div>
         </div>
       </nav>
-      {/* <nav className="navbar navbar-expand-lg">
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className={navSelection === "home" ? "nav-item active" : "nav-item"}>
-            <Link className="nav-link" to="/" onClick={() => setSelection("home")}>
-              Home
-              </Link>
-            </li>
-            <li className={navSelection === "projects" ? "nav-item active" : "nav-item"}>
-              <Link className="nav-link" to="/projects" onClick={() => setSelection("projects")}>
-              Projects
-              </Link>
-            </li>
-            <li className={navSelection === "interests" ? "nav-item active" : "nav-item"}>
-              <Link className="nav-link" to="/interests" onClick={() => setSelection("interests")}>
-              Interests
-              </Link>
-            </li>
-      
-            <li className="nav-item">
-            <a className="nav-link" href={Resume} download="Resume-PDF" target="_blank" rel="noreferrer">
-                Resume
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav> */}
     </div>
   );
 }
