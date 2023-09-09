3f60b84db5723f581ce6e267bf655696820c736d
Caleb Norris <paintballerz15@gmail.com>
2023-09-08 19:26:37 -0700

    Update Projects.tsx

--- src/components/main/Projects.tsx
+++ src/components/main/Projects.tsx
@@ -29,7 +29,8 @@ function Projects() {
             Quote calculation system giving customers a tool to get a detail
             quote for their automobile efficiently. Gives staff the ability to
             view quotes, pull quotes to a csv file for marketing, and to update
-            all pricing. Application was just launched and has generated more leads which has lead to more sales.{" "}
+            all pricing. Application was just launched and has generated more
+            leads which has lead to more sales.{" "}
           </p>
 
           <Carousel id="child_carousel">
@@ -54,11 +55,55 @@ function Projects() {
           </Carousel>
 
           <p className="">
-            Worked with client to plan, develop, and deploy a custom software solution
-            for their business. Building project from start to finish using the
-            MERN stack (MongoDB, Express, React, Node), it was deployed using Heroku. This is my college
-            capstone project along with it being a freelance project.
+            Worked with client to plan, develop, and deploy a custom software
+            solution for their business. Building project from start to finish
+            using the MERN stack (MongoDB, Express, React, Node), it was
+            deployed using Heroku. This is my college capstone project along
+            with it being a freelance project.
           </p>
+        </div>
+
+        <div className="project">
+          <h2>Movie Database (Practice Project)</h2>
+          <a
+            target="_blank"
+            href="https://github.com/CalebN99/Movies-R-Us"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} className="github-icon-projects" />
+            Github
+          </a>
+          <p>
+            Solo project practicing React and Typescript. Uses the open movie
+            database api. Pulls lists of movies by genre with default being most
+            popular, allows you to select genre, sort by popularity and rating,
+            and click on a movie to view a description and the rating of the
+            movie.
+          </p>
+
+          <Carousel id="child_carousel">
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={require("../../assets/movies/movie_1.png")}
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={require("../../assets/movies/movie_2.png")}
+                alt="Second slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={require("../../assets/movies/movie_3.png")}
+                alt="Third slide"
+              />
+            </Carousel.Item>
+          </Carousel>
         </div>
 
         <div className="project">
@@ -114,47 +159,6 @@ function Projects() {
             PHP), along with a php MVC framework (Fat-free). Presented project
             at the end of the quarter and received an 100% grade on the project.
           </p>
-        </div>
-        <div className="project">
-          <h2>Movie Database (Practice Project)</h2>
-          <a
-            target="_blank"
-            href="https://github.com/CalebN99/Movies-R-Us"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon icon={faGithub} className="github-icon-projects" />
-            Github
-          </a>
-          <p>
-            Solo project practicing React and Typescript. Uses the open movie database api.
-            Pulls lists of movies by genre with default being most popular, allows you to select genre, sort by
-            popularity and rating, and click on a movie to view a description
-            and the rating of the movie.
-          </p>
-
-          <Carousel id="child_carousel">
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src={require("../../assets/movies/movie_1.png")}
-                alt="First slide"
-              />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src={require("../../assets/movies/movie_2.png")}
-                alt="Second slide"
-              />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src={require("../../assets/movies/movie_3.png")}
-                alt="Third slide"
-              />
-            </Carousel.Item>
-          </Carousel>
         </div>
       </div>
     </div>
